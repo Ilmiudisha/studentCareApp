3372419fa8d6e4c31d380bc88d9f637733dc6957
Ilmi Udisha <139669406+Ilmiudisha@users.noreply.github.com>
2025-01-08 06:09:14 +0000

    Complete Login component with state management, user validation, and navigation functionality.

--- StudentCareApp/components/Login.js
+++ StudentCareApp/components/Login.js
@@ -10,6 +10,54 @@ import { useNavigation } from "@react-navigation/native";
 import { useState } from "react";
 
 export default function Login() {
-    
-    
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigation();
+
+    const handlebutton = () => {
+        // Checking for multiple students
+        const student = students.find(
+            (student) => student.username === name && student.password === password
+        );
+
+        if (student) {
+            navigate.navigate("profile", { studentdata: student });
+        } else {
+            alert("Invalid username or password.");
+        }
+    };
+    return (
+        <PaperProvider>
+            <ScrollView>
+                <View style={styles.header}>
+                    <Text style={styles.headerText}>UoV Student Care</Text>
+                </View>
+                <View style={styles.imagecontain}>
+                    <Image
+                        source={require("../assets/uovlogo.png")}
+                        style={styles.image}
+                    />
+                </View>
+                <View style={styles.container}>
+                    <Text style={styles.loginText}>Student Login</Text>
+                    <Divider />
+                </View>
+                <View style={styles.formcontainer}>
+                    <TextInput label="Username" mode="outlined" onChangeText={setName} />
+                    <TextInput
+                        label="Password"
+                        mode="outlined"
+                        secureTextEntry
+                        onChangeText={setPassword}
+                    />
+                    <View style={styles.buttoncontainer}>
+                        <Button mode="contained" onPress={handlebutton}>
+                            Login
+                        </Button>
+                    </View>
+                </View>
+            </ScrollView>
+
+        </PaperProvider>
+    );
 }
