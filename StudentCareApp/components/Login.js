dbb9ce8c6339004cec2d88f46a2208ab79b65f76
Ilmi Udisha <139669406+Ilmiudisha@users.noreply.github.com>
2025-01-08 06:07:18 +0000

    Skeleton setup for the Login component with necessary imports and structure.

---
+++ StudentCareApp/components/Login.js
@@ -0,0 +1,15 @@
+import { Image, ScrollView, StyleSheet, View } from "react-native";
+import {
+    Button,
+    Divider, PaperProvider, Text,
+    TextInput,
+} from "react-native-paper";
+
+import { students } from "../data/StudentsDb";
+import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
+
+export default function Login() {
+    
+    
+}
