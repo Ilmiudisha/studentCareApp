33a93885c0ccec30ed69c5afacc3797197c67cd1
Ilmi Udisha <139669406+Ilmiudisha@users.noreply.github.com>
2025-01-08 06:25:37 +0000

    Imports UI components from React Native and React Native Paper, and navigation functionality from React Navigation

---
+++ StudentCareApp/components/Profile.js
@@ -0,0 +1,9 @@
+import { Image, ScrollView, StyleSheet, View } from "react-native";
+import {
+    Button,
+    Divider,
+    PaperProvider,
+    Text,
+    TextInput,
+} from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
